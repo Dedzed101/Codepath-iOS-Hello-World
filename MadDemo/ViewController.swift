840d8f14280bccf5f74fcecc1c8bb1edb926a92d
Dedzed101 <59780347+Dedzed101@users.noreply.github.com>
2020-12-15 14:12:44 -0800

    CodePath iOS app

--- MadDemo/ViewController.swift
+++ MadDemo/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var backgroundColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        backgroundColor = view.backgroundColor
     }
 
+    @IBAction func DidTapButton(_ sender: Any) {
+        textLabel.textColor = UIColor.orange
+    }
+    
+    @IBAction func DidTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.red
+    }
 
+    @IBAction func DidTapTextButton(_ sender: Any) {
+        //textLabel.text = "Goodbye!"
+        textLabel.text = textField.text
+        textField.text = ""
+        view.endEditing(true)
+    }
+  
+    @IBAction func OnResetGesture(_ sender: Any) {
+        textLabel.text = "Hello from Daniel!"
+        view.backgroundColor = backgroundColor
+        textLabel.textColor = UIColor.black
+    }
+    
 }
 
